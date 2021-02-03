773cf093615581ecce46b5a0c03213e4028935a1
Mostary21 <fkfmkhan1@gmail.com>
2021-02-04 03:55:42 +0600

    module 23

---
+++ scope.js
@@ -0,0 +1,11 @@
+let bonus = 20;
+function sum(first, second){
+    let result = first + second + bonus;
+    console.log(result);
+    return result;
+}
+
+const output = sum(5, 15);
+console.log(output);
+// console.log(result);
+console.log(bonus);
