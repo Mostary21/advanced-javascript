3c52ed55384fc39c973acd585ddfecb09177df2b
Mostary21 <fkfmkhan1@gmail.com>
2021-02-03 06:02:30 +0600

    undefinef vs null

---
+++ undefined-null.js
@@ -0,0 +1,19 @@
+let bird;
+console.log(bird);
+function add (num1, num2){
+    console.log(num1 + num2);
+    return
+}
+
+function add(num1, num2){
+    console.log(num1, num2);
+}
+const result = add(13);
+console.log(result);
+
+const pagol = {name:"abcd", phone:420420};
+console.log(pagol.address);
+
+let fun = undefined;
+console.log(fun);
+
